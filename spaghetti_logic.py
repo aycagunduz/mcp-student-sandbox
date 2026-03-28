675a0736a0fc0255adf07740da2facbaca98a8f4
aycagunduz <fatmagndz760@gmail.com>
2026-03-29 01:52:53 +0300

    Refactor spaghetti logic into modular functions following SOLID principles

--- spaghetti_logic.py
+++ spaghetti_logic.py
@@ -1,10 +1,25 @@
-def process_data(data):
-    res = []
-    for d in data:
-        val = d * 1.15
-        s = f"Total: {val:.2f}"
-        print(s)
-        res.append(val)
-    with open("log.txt", "a") as f:
-        f.write(str(res) + "\n")
-    return res
+# Modüler fonksiyonlar: Her biri tek sorumluluk
+
+def apply_markup(value, markup_percent=15):
+    """Tek sorumluluk: Değere markup uygula"""
+    return value * (1 + markup_percent / 100)
+
+def format_currency(value):
+    """Tek sorumluluk: Para birimi formatı uygula"""
+    return f"Total: {value:.2f}"
+
+def log_result(result, filename="log.txt"):
+    """Tek sorumluluk: Sonucu dosyaya logla"""
+    with open(filename, "a") as f:
+        f.write(str(result) + "\n")
+
+def process_data(data, print_output=True):
+    """Ana fonksiyon: Küçük fonksiyonları orchestrate et"""
+    results = [apply_markup(d) for d in data]
+
+    if print_output:
+        for result in results:
+            print(format_currency(result))
+
+    log_result(results)
+    return results
